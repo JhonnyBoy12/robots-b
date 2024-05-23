06ef65dd240b5cd75a63bd5fef3b318320ac2728
Jhonny Silva <matias.silva.200517@gmail.com>
2024-05-23 12:01:24 -0400

    comit

---
+++ controllers/getrobots.js
@@ -0,0 +1,41 @@
+const {request, response} = require("express");
+
+
+const { robots } = require("../robots");
+
+const getRobots = (req= request, res= response)=>{
+    res.json({
+        status:true,
+        robots,
+        statuscode:200
+    });
+}
+
+const getRobotsById = (req= request, res= response)=>{
+
+    let id = (req.params.id);
+
+    let robotS = "";
+    robotS = robots.find( ( robot )=>{
+        return robot.id === id;
+
+    });
+    if (robotS){
+        return res.json({
+            ok:true,
+            robotS,
+            statusCode:200
+        });
+    }else{
+        return res.json({
+            ok:false,
+            msg:"no hay robot",
+            statusCode:404
+        });
+    }
+
+}
+module.exports = {
+    getRobots,
+    getRobotsById
+}
